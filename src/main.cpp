0edd90d5c8c20e6774de60f229c96a6ded4ac487
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2014-10-26 09:45:36 +0100

    Fix Qt conventions I do not care for

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -8,7 +8,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget* parent = 0);
     ~MainWindow();
 };
 
