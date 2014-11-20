f33be42a1bd5d811567da11c5cfa3d9183f74e8e
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2014-11-20 16:57:20 +0100

    First iteration of menus and controls

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -22,13 +22,19 @@
 
 #include <QLabel>
 #include <QStatusBar>
+#include <QAction>
+#include <QMenuBar>
+#include <QMenu>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 {
   centralWidget = new MainCentralWidget;
   setCentralWidget(centralWidget);
 
+  createActions();
+
   createStatusBar();
+  createMenus();
 
   setMinimumSize(800, 600);
 }
@@ -45,4 +51,50 @@ void MainWindow::createStatusBar()
   statusBar()->addWidget(statusBarGameLabel);
   statusBar()->addWidget(statusBarCurrentGame);
   statusBar()->setSizeGripEnabled(false);
+}
+
+void MainWindow::createActions()
+{
+  programAboutAction = new QAction(tr("About"), this);
+  programAboutAction->setStatusTip(tr("About Zeitgeist"));
+
+  programSettingsAction = new QAction(tr("Settings"), this);
+  programSettingsAction->setStatusTip(tr("Edit program settings"));
+
+  programQuitAction = new QAction(tr("Quit"), this);
+
+  gameEditAction = new QAction(tr("Edit"), this);
+  gameEditAction->setStatusTip(tr("Edit games"));
+
+  gameRefreshAction = new QAction(tr("Refresh"), this);
+  gameRefreshAction->setStatusTip(tr("Refresh game"));
+
+  gameInstallAction = new QAction(tr("Install"), this);
+  gameInstallAction->setStatusTip(tr("Queue a mod for installation"));
+
+  gameUnqueueAction = new QAction(tr("Unqueue"), this);
+  gameUnqueueAction->setStatusTip(tr("Remove a mod from the queue"));
+
+  gameUninstallAction = new QAction(tr("Uninstall"), this);
+  gameUninstallAction->setStatusTip(tr("Queue a mod for uninstallation"));
+
+  gameProcessAction = new QAction(tr("Process"), this);
+  gameProcessAction->setStatusTip(tr("Process queue"));
+}
+
+void MainWindow::createMenus()
+{
+  programMenu = menuBar()->addMenu(tr("Program"));
+  programMenu->addAction(programSettingsAction);
+  programMenu->addAction(programAboutAction);
+  programMenu->addAction(programQuitAction);
+
+  gameMenu = menuBar()->addMenu(tr("Game"));
+  gameMenu->addAction(gameEditAction);
+  // Recent (games)
+  gameMenu->addAction(gameRefreshAction);
+  gameMenu->addAction(gameInstallAction);
+  gameMenu->addAction(gameUnqueueAction);
+  gameMenu->addAction(gameUninstallAction);
+  gameMenu->addAction(gameProcessAction);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,6 +5,8 @@
 
 class MainCentralWidget;
 class QLabel;
+class QAction;
+class QMenu;
 
 class MainWindow : public QMainWindow
 {
@@ -13,12 +15,26 @@ class MainWindow : public QMainWindow
  public:
   MainWindow(QWidget* parent = 0);
   ~MainWindow();
-  void createStatusBar();
 
  private:
+  void createStatusBar();
+  void createActions();
+  void createMenus();
+
   MainCentralWidget* centralWidget;
   QLabel* statusBarGameLabel;
   QLabel* statusBarCurrentGame;
+  QAction* programAboutAction;
+  QAction* programSettingsAction;
+  QAction* programQuitAction;
+  QAction* gameEditAction;
+  QAction* gameRefreshAction;
+  QAction* gameInstallAction;
+  QAction* gameUnqueueAction;
+  QAction* gameUninstallAction;
+  QAction* gameProcessAction;
+  QMenu* programMenu;
+  QMenu* gameMenu;
 };
 
 #endif // MAINWINDOW_H
